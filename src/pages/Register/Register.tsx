907d4e6e2e2c06bd3774f8356b97c819234d9c03
florian.bar <florian.bar@muvraline.fr>
2026-03-13 15:27:29 +0100

    Fix deployement imports

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AuthForm } from '../../components/AuthForm/AuthForm'
 import { useAuth } from '../../hooks/useAuth'
